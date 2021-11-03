c22a8bae1133fea2f2f77e2014d9e17f2d80e96b
kena0ki <ken3574.25@gmail.com>
2021-11-03 15:38:03 +0900

    redirect

--- server/proxy.ts
+++ server/proxy.ts
@@ -6,20 +6,31 @@ const server = http.createServer((req,res) => {
   res.setHeader('access-control-allow-origin', '*');
   if (!req.url) return res.end();
   const target = 'https:/'+req.url;
-  try {
-    const proxyReq = https.request(target, proxyRes => {
-      console.log(`statusCode: ${proxyRes.statusCode}, target: ${target}`);
-      proxyRes.pipe(res);
-    });
-    proxyReq.on('error', error => {
-      console.error('error:', error);
-    });
-    proxyReq.end(); // terminate our message request. otherwise, servers would keep waiting for our messages.
-  } catch(e) {
-    console.error('cause:', e);
-    res.statusCode=500;
-    res.end();
-  }
+  const MAX_REDIRECTS=10;
+  let redirect_cnt=0;
+  const loop = (target: string) => {
+    try {
+      const proxyReq = https.request(target, proxyRes => {
+        console.log(`statusCode: ${proxyRes.statusCode}, target: ${target}`);
+        const redirectUrl = proxyRes.headers.location;
+        if (redirectUrl && redirect_cnt < MAX_REDIRECTS) {
+          redirect_cnt++;
+          loop(redirectUrl);
+        } else {
+          proxyRes.pipe(res);
+        }
+      });
+      proxyReq.on('error', error => {
+        console.error('error:', error);
+      });
+      proxyReq.end(); // terminate our message request. otherwise, servers would keep waiting for our messages.
+    } catch(e) {
+      console.error('cause:', e);
+      res.statusCode=500;
+      res.end();
+    }
+  };
+  loop(target);
 });
 server.on('listening', () =>{
   console.log('listening on port', PORT);
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const local = location.protocol + '//' + location.hostname + ':5001';
+const local = location.protocol + '//' + location.hostname + ':3051';
 const PROXY_HOST = DEBUG ? local: 'https://cors.znoo.xyz';
 export default {
   PROXY_HOST,
